2289a60f6c72cef21faad48c25618af58581b36f
AlonsoMartinToledano <alonsomtgm@gmail.com>
2020-03-15 19:17:32 +0100

    Ej 5 finished

--- corba/matriculas-detector/matriculas-detector.py
+++ corba/matriculas-detector/matriculas-detector.py
@@ -7,4 +7,10 @@ img = cv2.imread('matricula.jpg')
 text = pytesseract.image_to_string(img)
 
 matricula = text [1:len(text)]
+
+f=open("matriculas.txt","w")
+f.write(matricula + "\n")
+
+f.close()
+
 print(matricula)
