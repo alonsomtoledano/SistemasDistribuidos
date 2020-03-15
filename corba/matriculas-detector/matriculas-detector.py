e7f82cc093be7122c2f123ed48d24e18968a098c
AlonsoMartinToledano <alonsomtgm@gmail.com>
2020-03-15 13:56:57 +0100

    Ej5

---
+++ corba/matriculas-detector/matriculas-detector.py
@@ -0,0 +1,10 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+img = cv2.imread('matricula.jpg')
+text = pytesseract.image_to_string(img)
+
+matricula = text [1:len(text)]
+print(matricula)
